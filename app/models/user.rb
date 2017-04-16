fa7d8e32d9b9bdd92602fd32c460f6becef76830
mrfirki <mrfikri94@gmail.com>
2017-04-16 13:53:53 +0800

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+require 'bcrypt'
+class User < ApplicationRecord
+	has_secure_password
+
+	validates :username, presence: true, length: { maximum: 50 }
+	validates :email, presence: true,  uniqueness: true
+	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+end
